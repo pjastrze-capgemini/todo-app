bc2fe734bdd772a4ff6d480e180b240347803381
pjastrze <pjastrze@cap.com>
2025-11-24 14:00:52 +0100

    added register form

--- angular-frontend/src/app/components/register-form/register-form.ts
+++ angular-frontend/src/app/components/register-form/register-form.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MessageModule } from 'primeng/message';
+import { Button } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-register-form',
-  imports: [],
+  imports: [CardModule, Button, InputTextModule, FloatLabelModule, FormsModule, ReactiveFormsModule, RouterModule, MessageModule],
   templateUrl: './register-form.html',
   styleUrl: './register-form.scss',
 })
 export class RegisterForm {
+  registerForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      login: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    });
+  }
+
+  isInvalid(controlName: string) {
+    const control = this.registerForm.get(controlName);
+    return control?.invalid && control.touched;
+  }
+
+  onRegister(): void {
+    if (this.registerForm.valid) {
+      const { login, password, confirmPassword } = this.registerForm.value;
+      console.log('Login:', login);
+      console.log('Password:', password);
+      console.log('ConfirmPassword:', confirmPassword);
+    }
+  }
 }
